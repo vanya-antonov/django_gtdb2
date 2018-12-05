bf6c1d3541fb11c21dd103c3269c291957f79b57
vanya-antonov <vanya.antonov@gmail.com>
2018-12-05 20:40:42 +0300

    get_full_path_to(), make_tmp_dir() and __del__() methods were added

--- gtdb2/lib/db.py
+++ gtdb2/lib/db.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import shutil
+import tempfile
 
 from django.conf import settings
 
@@ -13,6 +14,12 @@ class GeneTackDB:
     def __init__(self):
         """Initialize with a path to GTDB root dir on the local machine."""
         self.root_dir = settings.GTDB_DIR
+        self.all_tmp_dirs = []
+
+    def __del__(self):
+        "Make sure the delete all created tmp dirs and files."
+        for path in self.all_tmp_dirs:
+            shutil.rmtree(path)
 
     def get_default_user(self):
         """Returns 'default' user for manage.py commands."""
@@ -20,6 +27,10 @@ class GeneTackDB:
             name='gtdb_manage.py',
             descr='Default GeneTackDB user')
         return user
+
+    def get_full_path_to(self, *args):
+        "Returns a full path for a GTDB dir relative path."
+        return os.path.join(self.root_dir, *args)
 
     def delete_file(self, fn):
         """Argument is the path RELATIVE to the GTDB dir."""
@@ -36,4 +47,13 @@ class GeneTackDB:
             shutil.rmtree(path)
         else:
             logging.warning("Folder '%s' doesn't exist!" % path)
+
+    def make_tmp_dir(self, keep=False):
+        "Creates a new tmp folder in GTDB dir and returns its relative path."
+        subdir = os.path.join(self.root_dir, 'tmp')
+        os.makedirs(subdir, exist_ok=True)
+        tmp_path = tempfile.mkdtemp(prefix='__gtdb.', dir=subdir)
+        if not keep:
+            self.all_tmp_dirs.append(tmp_path)
+        return os.path.relpath(tmp_path, self.root_dir)
 
