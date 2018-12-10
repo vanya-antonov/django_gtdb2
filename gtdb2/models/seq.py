92c3f3ae3d8c8f29834636e83b28ce02801d8266
vanya-antonov <vanya.antonov@gmail.com>
2018-12-10 21:52:20 +0300

    Seq.create_from_ext_id(), Seq.as_SeqRecord() and corresponding tests were added

--- gtdb2/models/seq.py
+++ gtdb2/models/seq.py
@@ -1,4 +1,10 @@
+# Copyright 2018 by Ivan Antonov. All rights reserved.
+
 from django.db import models
+
+import Bio
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
 
 from gtdb2.models.abstract import AbstractUnit, AbstractParam
 from .org import Org
@@ -13,6 +19,28 @@ class Seq(AbstractUnit):
 
     class Meta:
         db_table = 'seqs'
+
+    def as_SeqRecord(self, with_seq=False):
+        "Returns SeqRecord object with seq from fasta file if requested."
+        if with_seq:
+            fasta_fn = self.org.get_full_path_to('seq_fna', self.id)
+            seq = SeqIO.read(fasta_fn, "fasta").seq
+        else:
+            seq = Bio.Seq.UnknownSeq(self.len)
+        return SeqRecord(
+            seq, id=self.id, name=self.name, description=self.descr,
+            annotations={'molecule_type': self.type})
+
+    @classmethod
+    def create_from_ext_id(cls, user, org, ext_id):
+        "Reads gbk file from org_dir and creates seq from SeqRecord."
+        gbk_fn = org.get_full_path_to('seq_gbk', ext_id)
+        record = SeqIO.read(gbk_fn, "genbank")
+        seq = cls(user=user, org=org,
+                  id=record.id, name=record.name, descr=record.description,
+                  type=record.annotations['molecule_type'], len=len(record.seq))
+        seq.save()
+        return seq
 
 
 class SeqParam(AbstractParam):
---
+++ gtdb2/tests/test_seq.py
@@ -0,0 +1,29 @@
+# Copyright 2018 by Ivan Antonov. All rights reserved.
+
+from gtdb2.lib.db import GeneTackDB
+from gtdb2.models.org import Org
+from gtdb2.models.seq import Seq
+from gtdb2.tests import GtdbTestCase
+
+
+class SeqModelTests(GtdbTestCase):
+    def test_seq_create_from_ext_id(self):
+        gtdb = GeneTackDB()
+        user = gtdb.get_default_user()
+        fn = self.get_full_path_to_test_file('N_mexicana.gbk')
+
+        # All the seq files will be created in a temporary folder
+        Org.subdir = gtdb.make_tmp_dir()
+        org = Org.create_from_gbk(user, fn)
+
+        seq = Seq.create_from_ext_id(user, org, 'NZ_BDBV01000005.1')
+
+        self.assertEqual(seq.id, 'NZ_BDBV01000005.1')
+        self.assertEqual(seq.name, 'NZ_BDBV01000005')
+        self.assertEqual(seq.type, 'DNA')
+        self.assertEqual(seq.len, 34847)
+
+        # Make sure seq can be read from file
+        record = seq.as_SeqRecord(with_seq=True)
+        self.assertTrue(record.seq.startswith('GAAGCGATAGCCAAGCAACTACA'))
+
