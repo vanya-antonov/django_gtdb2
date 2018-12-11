e313b43b5b8cfa27fc85936bfce7eabcacb52bbb
vanya-antonov <vanya.antonov@gmail.com>
2018-12-11 20:48:34 +0300

    Fshift create_from_gtdb1_fs() was implemented

--- gtdb2/models/fshift.py
+++ gtdb2/models/fshift.py
@@ -1,5 +1,6 @@
 # Copyright 2018 by Ivan Antonov. All rights reserved.
 
+import re
 from pprint import pprint
 
 from django.db import models
@@ -10,9 +11,9 @@ from gtdb2.models.seq import Seq
 
 class FShift(AbstractUnit):
     seq = models.ForeignKey(Seq, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255)
     coord = models.IntegerField()
-    type = models.IntegerField()
-    origin = models.CharField(max_length=255)
+    len = models.IntegerField()
     start = models.IntegerField()
     end = models.IntegerField()
     strand = models.IntegerField()
@@ -21,82 +22,20 @@ class FShift(AbstractUnit):
         db_table = 'fshifts'
 
     @classmethod
-    def create_from_gtdb1_fs(cls, user, org, gtdb1_fs):
-        if gtdb1_fs.job.species not in org.name:
-            raise ValueError("Provided gtdb1_fs and org do not match!")
+    def create_from_gtdb1_fs(cls, user, seq, gtdb1_fs):
+        "Creates Fshift based on the info from GTDB1 fs."
+        fs = _validate_gtdb1_fs(gtdb1_fs, seq)
 
-#        pprint(vars(gtdb1_fs))
-#        pprint(vars(gtdb1_fs.job))
-#        pprint(vars(org))
+        fshift = FShift(
+            user=user, seq=seq, c_date=fs.job.c_date, type='genetack',
+            name=_get_fshift_name(seq, fs.fs_coord, int(fs.type)),
+            descr=fs.descr, coord=fs.fs_coord, len=int(fs.type),
+            start=fs.start, end=fs.end, strand=fs.strand)
+        fshift.save()
 
-'''
-def create_new_in_db_from_GT_FS(gtdb, fs_id, source='genetack'):
-    fs, seq = _get_info_about_GT_FS(gtdb, fs_id)
-    
-    # If up_len_nt is not divisible by 3 => the FS was predicted in a middle of codon.
-    # Move the fs-coord upstream to avoid stop codon in cases like 'aaa_tgA_CCC'.
-    adjust_len = fs['up_len_nt'] % 3
-    if fs['strand'] == 1:
-        fs['fs_coord'] -= adjust_len
-    else:
-        fs['fs_coord'] += adjust_len
-    
-    fsgene = FSGene.create_new_in_db(
-        gtdb, seq.id, fs['user_id'], fs['start'], fs['end'], fs['strand'], fs['fs_coord'], fs['type'],
-        source=source, cof_id=fs['cof_id'], c_date=fs['c_date'], db_id=fs_id)
-    fsgene.make_all_params()
-    
-def _get_info_about_GT_FS(gtdb, fs_id):
-    res = gtdb.exec_sql_ar("""
-        select f.fs_id, j.user_id, j.c_date, s.id AS seq_id, s.ext_id AS seq_ext_id,
-               f.fs_coord, f.type, f.init_gene_seq,
-               IF(f.strand = "+", 1, -1) AS strand,
-               (select cof_id from cof_gtfs cg where cg.fs_id = f.fs_id limit 1) AS cof_id
-        from gt_fs f, jobs j, seqs s
-        where j.job_id=f.job_id
-        and s.name = j.name
-        and f.fs_id = %s
-    """, fs_id
-    )
-    if len(res) == 0:
-        logging.warning("Can't get info about FS_ID = '%s'" % fs_id)
-        return None
-    elif not res[0]['seq_ext_id'].endswith('.1'):
-        logging.warning("Sequence '%s' has wrong version!" % res[0]['seq_ext_id'])
-        return None
-    else:
-        fs = res[0]
-        fs['type'] = int(fs['type'])
-    
-    if fs['strand'] == -1:
-        fs['fs_coord'] -= 1   # switch to zero-based coordinate system?
-    
-    up_match = re.compile('^[a-z]+').search(fs['init_gene_seq'])
-    down_match = re.compile('[A-Z]+$').search(fs['init_gene_seq'])
-    if up_match is None or down_match is None:
-        logging.warning("Wrong fsgene sequence '%s'" % fs['init_gene_seq'])
-        return None
-    fs['up_len_nt'] = up_match.end() - up_match.start()
-    fs['down_len_nt'] = down_match.end() - down_match.start()
-    
-    if fs['strand'] == 1:
-        fs['start'] = fs['fs_coord'] - fs['up_len_nt']
-        fs['end'] = fs['fs_coord'] + fs['down_len_nt']
-    else:
-        fs['start'] = fs['fs_coord'] - fs['down_len_nt']
-        fs['end'] = fs['fs_coord'] + fs['up_len_nt']
-    
-    seq = SeqWithSeq(gtdb, fs['seq_id'])
-    fsgene_seq = seq[fs['start']:fs['end']]
-    if fs['strand'] == -1:
-        fsgene_seq = fsgene_seq.reverse_complement()
-    
-    if fsgene_seq.upper() != fs['init_gene_seq'].upper():
-        logging.warning("GT_FS and fsgene sequence do not match:\n%s\n%s" % (fs['init_gene_seq'], fsgene_seq))
-        return None
-    
-    return fs, seq
-'''
+        # TODO: add gtdb1 ID to xrefs
+
+        return fshift
 
 
 class FShiftParam(AbstractParam):
@@ -104,4 +43,59 @@ class FShiftParam(AbstractParam):
 
     class Meta:
         db_table = 'fshift_params'
+
+
+def _get_fshift_name(seq, fs_coord, fs_len):
+    "Generates name like: 'NC_010002.1:1922457:-1'."
+    if fs_len == 0:
+        # to avoid 'NC_005085.1:1684916:+0'
+        return '%s:%d:%d' % (seq.id, fs_coord, fs_len)
+    else:
+        return '%s:%d:%+d' % (seq.id, fs_coord, fs_len)
+
+def _validate_gtdb1_fs(fs, seq):
+    """Makes sure that GTDB1 fs matches GTDB2 seq and modifies/adds some
+    attributes to the fs object.
+    """
+    if fs.job.species not in seq.org.name:
+        raise ValueError("Provided gtdb1_fs.species and seq.org do not match:"
+                         "\n%s\n%s" %(fs.job.species, seq.org.name))
+
+    fs.strand = -1 if fs.strand == '-' else 1
+
+    if fs.strand == -1:
+        fs.fs_coord -= 1   # switch to zero-based coordinate system
+
+    up_match = re.compile('^[a-z]+').search(fs.init_gene_seq)
+    down_match = re.compile('[A-Z]+$').search(fs.init_gene_seq)
+    if up_match is None or down_match is None:
+        raise ValueError("Wrong fsgene sequence '%s'" % fs.init_gene_seq)
+    fs.up_len_nt = up_match.end() - up_match.start()
+    fs.down_len_nt = down_match.end() - down_match.start()
+
+    if fs.strand == 1:
+        fs.start = fs.fs_coord - fs.up_len_nt
+        fs.end = fs.fs_coord + fs.down_len_nt
+    else:
+        fs.start = fs.fs_coord - fs.down_len_nt
+        fs.end = fs.fs_coord + fs.up_len_nt
+
+    fsgene_seq = seq.record.seq[fs.start:fs.end]
+    if fs.strand == -1:
+        fsgene_seq = fsgene_seq.reverse_complement()
+
+    if fsgene_seq.upper() != fs.init_gene_seq.upper():
+        raise ValueError("GT_FS and fsgene sequences do not match:\n%s\n%s" %
+                         (fs['init_gene_seq'], fsgene_seq))
+
+    # If up_len_nt is not divisible by 3 => the FS was predicted in a
+    # middle of a codon => Move the fs-coord upstream to avoid stop
+    # codon in cases like 'aaa_tgA_CCC'.
+    adjust_len = fs.up_len_nt % 3
+    if fs.strand == 1:
+        fs.fs_coord -= adjust_len
+    else:
+        fs.fs_coord += adjust_len
+
+    return fs
 
---
+++ gtdb2/tests/test_fshift.py
@@ -0,0 +1,54 @@
+# Copyright 2018 by Ivan Antonov. All rights reserved.
+
+import pickle
+from pprint import pprint
+
+from gtdb2.lib.db import GeneTackDB
+from gtdb2.models.fshift import FShift
+from gtdb2.models.org import Org
+from gtdb2.models.seq import Seq
+from gtdb2.tests import GtdbTestCase
+
+
+class FShiftModelTests(GtdbTestCase):
+    def test_fshift_create_from_gtdb1(self):
+        """While testing I can't get access to the data in any production
+        database. So, the gtdb1_fs object was stored in a pickle file that
+        was created as follows:
+        cd ~/_my/github/django_gtdb2
+        python3 manage.py shell
+        >>> import pickle
+        >>> from gtdb1.models import GtFs
+        >>> gtdb1_id = 33540948
+        >>> gtdb1_fs = GtFs.objects.using('gtdb1').get(pk=gtdb1_id)
+        >>> job = gtdb1_fs.job
+        >>> out_fn = 'gtdb2/tests/data/gtdb1_fs_%s.pickle' % gtdb1_id
+        >>> with open(out_fn, 'wb') as handle:
+        >>>     pickle.dump(gtdb1_fs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        """
+        gtdb = GeneTackDB()
+        user = gtdb.get_default_user()
+
+        # All the seq files will be created in a temporary folder
+        Org.subdir = gtdb.make_tmp_dir()
+
+        # Prepare required objects
+        gbk_fn = self.get_full_path_to_test_file('M_fervens.gbk')
+        org = Org.create_from_gbk(user, gbk_fn)
+        seq = Seq.create_from_ext_id(user, org, 'NC_013156.1')
+        fs_fn = self.get_full_path_to_test_file('gtdb1_fs_33540948.pickle')
+        with open(fs_fn, 'rb') as handle:
+            gtdb1_fs = pickle.load(handle)
+
+        fshift = FShift.create_from_gtdb1_fs(user, seq, gtdb1_fs)
+
+        # Make sure the old date was preserved
+        self.assertTrue(fshift.c_date.year == 2010)
+
+        # Validate fs attributes
+        self.assertEqual(fshift.name, 'NC_013156.1:1122957:-1')
+        self.assertEqual(fshift.type, 'genetack')
+        self.assertEqual(fshift.coord, 1122957)
+        self.assertEqual(fshift.len, -1)
+        self.assertEqual(fshift.strand, 1)
+
