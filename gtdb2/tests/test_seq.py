8e196c31421bab075480fcb53b40c87a4eb7fca8
vanya-antonov <vanya.antonov@gmail.com>
2018-12-11 20:38:20 +0300

    validate c_date

--- gtdb2/tests/test_seq.py
+++ gtdb2/tests/test_seq.py
@@ -1,5 +1,7 @@
 # Copyright 2018 by Ivan Antonov. All rights reserved.
 
+from datetime import datetime
+
 from gtdb2.lib.db import GeneTackDB
 from gtdb2.models.org import Org
 from gtdb2.models.seq import Seq
@@ -18,13 +20,15 @@ class SeqModelTests(GtdbTestCase):
 
         seq = Seq.create_from_ext_id(user, org, 'NZ_BDBV01000005.1')
 
-        # Check created params
-        self.assertTrue('gbk_fn' in seq.prm)
-
+        # Check attributes
+        self.assertEqual(seq.c_date.year, datetime.now().year)
         self.assertEqual(seq.id, 'NZ_BDBV01000005.1')
         self.assertEqual(seq.name, 'NZ_BDBV01000005')
         self.assertEqual(seq.type, 'DNA')
         self.assertEqual(seq.len, 34847)
+
+        # Check created params
+        self.assertTrue('gbk_fn' in seq.prm)
 
         # Make sure seq can be read from file
         self.assertTrue(seq.record.seq.startswith('GAAGCGATAGCCAAGCAACTACA'))
