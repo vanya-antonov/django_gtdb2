348ce5519b1872ac910a99b28db553b0a5fb170e
vanya-antonov <vanya.antonov@gmail.com>
2018-12-21 20:12:58 +0300

    Set GTDB_DIR to tmp dir while running the tests

--- gtdb2/tests/__init__.py
+++ gtdb2/tests/__init__.py
@@ -1,13 +1,19 @@
 # Copyright 2018 by Ivan Antonov. All rights reserved.
 
+import logging
 import os
+import shutil
+import tempfile
 
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from gtdb2.lib.db import GeneTackDB
 from gtdb2.models.org import Org
 
+# All the files (if any) will be created in a tmp dir
+# https://docs.djangoproject.com/en/2.1/topics/testing/tools/#overriding-settings
+@override_settings(GTDB_DIR=tempfile.mkdtemp(prefix='__gtdb.'))
 
 class GtdbTestCase(TestCase):
     """This class add some commonly used attributes and provides some
@@ -20,9 +26,16 @@ class GtdbTestCase(TestCase):
         self.gtdb = GeneTackDB()
         self.user = self.gtdb.get_or_create_default_user()
 
-        # All the seq files (if any) will be created in a temporary folder
-        #Org.subdir = gtdb.make_tmp_dir(keep=True)
-        Org.subdir = self.gtdb.make_tmp_dir()
+    def tearDown(self):
+        "Deletes tmp dir."
+        # Make sure that we delete tmp folder and not the real GTDB_DIR
+        if '__gtdb.' in settings.GTDB_DIR:
+            shutil.rmtree(settings.GTDB_DIR)
+            logging.debug("Tmp folder '%s' has been removed." %
+                          settings.GTDB_DIR)
+        else:
+            logging.warning("GTDB_DIR is NOT a tmp folder '%s' in testing!" %
+                            settings.GTDB_DIR)
 
     def get_full_path_to_test_file(self, fn):
         return os.path.join(settings.BASE_DIR, self.test_data_dir, fn)
