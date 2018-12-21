99963c9a53c4772a327c4a27d89a02a48d20ab7d
vanya-antonov <vanya.antonov@gmail.com>
2018-12-21 20:16:18 +0300

    Make sure the path is relative and the file exists

--- gtdb2/tests/test_seq.py
+++ gtdb2/tests/test_seq.py
@@ -1,6 +1,7 @@
 # Copyright 2018 by Ivan Antonov. All rights reserved.
 
 from datetime import datetime
+import os
 
 from gtdb2.lib.db import GeneTackDB
 from gtdb2.models.org import Org
@@ -24,6 +25,14 @@ class SeqModelTests(GtdbTestCase):
 
         # Check created params
         self.assertTrue('gbk_fn' in seq.prm)
+
+        # We must save relative path to the file
+        rel_path = 'orgs/Nocardia_mexicana_NBRC_108244/seq_gbk/NZ_BDBV01000005.1'
+        self.assertEqual(seq.prm['gbk_fn'], rel_path)
+
+        # Make sure the file exists
+        gbk_fn = self.gtdb.get_full_path_to(seq.prm['gbk_fn'])
+        self.assertTrue(os.path.exists(gbk_fn) and os.path.getsize(gbk_fn) > 0)
 
         # Make sure seq can be read from file
         self.assertTrue(seq.record.seq.startswith('GAAGCGATAGCCAAGCAACTACA'))
