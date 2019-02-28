92a8482d12d68ab530c89d4226d85adc8a07877b
vanya-antonov <vanya.antonov@gmail.com>
2019-02-28 19:26:40 +0300

    Class-based views for Org list and Org details were created
    https://www.youtube.com/watch?v=-s7e_Fy6NRU

--- gtdb2/urls.py
+++ gtdb2/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index')]
+    path('', views.OrgListView.as_view(), name='gtdb2-home'),
+    path('org/<int:pk>/', views.OrgDetailView.as_view(), name='gtdb2-org-detail'),
+    path('about/', views.about, name='gtdb2-about'),
+]
 
--- gtdb2/views.py
+++ gtdb2/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
-def index(request):
-    return HttpResponse("<h2>Hey!<h2>")
+from .models import Org
+
+
+class OrgListView(ListView):
+    model = Org
+    # These are Django defaults that can be changed if needed
+    # template_name = 'gtdb2/org_list.html'
+    # context_object_name = 'object_list'
+    ordering = ['name']
+
+
+class OrgDetailView(DetailView):
+    model = Org
+
+
+def about(request):
+    return render(request, 'gtdb2/about.html', {'title': 'About'})
 
